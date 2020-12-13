cb0d6d38726dbe4bd310290e6528c45f1e439795
Azis Muttaqin <azis.muttaqin11@gmail.com>
2020-12-13 13:34:57 +0700

    modify logo, nav bar, adn create image for dashboard

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,7 +1,10 @@
 <div class="grid_12">
     <div class="box round first fullpage">
-        <h2>Home</h2>
-        <div class="block "></div>
+        </br></br>
+        <center>
+            <img src="<?= BASEURL; ?>/img/go-anzen-ni.png" alt="GO ANZEN NI" />
+        </center>
+        </br></br></br>
     </div>
 </div>
 
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,15 +8,17 @@
 
     <title> <?= $data['judul']; ?> </title>
 
-    <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/all.css" media="screen" />
 
-    <link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/reset.css" media="screen" />
 
-    <link rel="stylesheet" type="text/css" href="css/grid.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/text.css" media="screen" />
 
-    <link rel="stylesheet" type="text/css" href="css/layout.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/grid.css" media="screen" />
 
-    <link rel="stylesheet" type="text/css" href="css/nav.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/layout.css" media="screen" />
+
+    <link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/nav.css" media="screen" />
 
     <!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" /><![endif]-->
 
@@ -32,7 +34,7 @@
 
     <script src="<?= BASEURL; ?>/js/jquery-1.6.4.min.js" type="text/javascript"></script>
 
-    <script type="text/javascript" src="js/jquery-ui/jquery.ui.core.min.js"></script>
+    <script type="text/javascript" src="<?= BASEURL; ?>/js/jquery-ui/jquery.ui.core.min.js"></script>
 
     <script src="<?= BASEURL; ?>/js/jquery-ui/jquery.ui.widget.min.js" type="text/javascript"></script>
 
@@ -145,8 +147,8 @@
             <div id="branding">
 
                 <div class="floatleft">
-
-                    <img src="img/logo.png" alt="Logo" /></div>
+                    
+                    <img src="img/logo2.png" alt="Logo" /></div>
 
                 <div class="floatright">
 
@@ -189,48 +191,10 @@
         <div class="grid_12">
 
              <ul class="nav main">
-
-                <li class="ic-dashboard"><a href="dashboard.html"><span>Dashboard</span></a> </li>
-
-                <li class="ic-form-style"><a href="javascript:"><span>Controls</span></a>
-
-                    <ul>
-
-                        <li><a href="form-controls.html" >Forms</a> </li>
-
-                        <li><a href="buttons.html">Buttons</a> </li>
-
-                        <li><a href="form-controls.html">Full Page Example</a> </li>
-
-                        <li><a href="table.html">Page with Sidebar Example</a> </li>
-
-                    </ul>
-
-                </li>
-
-				<li class="ic-typography"><a href="typography.html"><span>Typography</span></a></li>
-
-                <li class="ic-charts"><a href="charts.html"><span>Charts & Graphs</span></a></li>
-
-                <li class="ic-grid-tables"><a href="table.html"><span>Data Table</span></a></li>
-
-                <li class="ic-gallery dd"><a href="javascript:"><span>Image Galleries</span></a>
-
-               		 <ul>
-
-                        <li><a href="image-gallery.html">Pretty Photo</a> </li>
-
-                        <li><a href="gallery-with-filter.html">Gallery with Filter</a> </li>
-
-                    </ul>
-
-                </li>
-
-                <li class="ic-notifications"><a href="notifications.html"><span>Notifications</span></a></li>
-
-
-
-            </ul>
+             <li><a class="active" href="#"><i class="fa fa-fw fa-home fa-lg"></i> Dashboard</a></li>
+             <li><a class="active" href="#"><i class="fa fa-fw fa-cloud-upload-alt fa-lg"></i> Loading</a></li>
+             <li><a class="active" href="#"><i class="fa fa-fw fa-cloud-download-alt fa-lg"></i> Unloading</a></li>
+             <li><a class="active" href="#"><i class="fa fa-fw fa-cloud-sun fa-lg"></i> Drying</a></li>
 
         </div>
 
