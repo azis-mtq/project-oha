bee954a7f149e187ca626088748ea6db0e7249c9
Azis Muttaqin <azis.muttaqin11@gmail.com>
2020-12-19 01:51:27 +0700

    edit text at footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -6,7 +6,7 @@
 
         <p>
 
-            Copyright <a href="#">BlueWhale Admin</a>. All Rights Reserved.
+            Copyright &copy; <script>document.write(new Date().getFullYear());</script>. All Rights Reserved.
 
         </p>
 
